f20c9ebb26126025d6eb11c8f975b50aa15308af
gitSnigel <romanov.william@gmail.com>
2013-02-24 19:19:19 +0100

    method missing

--- Array.rb
+++ Array.rb
@@ -20,6 +20,7 @@ class Array
       }
       end
    else
+      puts argument
       min = argument.values.first.values.first
       max = argument.values.first.values.last
       self.each{
@@ -37,6 +38,23 @@ class Array
     return  select_all(args)[0]
   end #select_first
 
+  def method_missing(name, *args)
+    if attribute = name.to_s.match(/^select_first_where_(.*)_is$/)
+     puts "vad blir under "
+       select_first(attribute[1].to_sym => args)
+    elsif attribute = name.to_s.match(/^select_first_where_(.*)_is_in$/)
+      puts "elsif"
+      select_first(:name => attribute[1].to_sym, :interval => {:min => args[0], :max => args[1]})
+    elsif  attribute = name.to_s.match(/^select_all_where_(.*)_is$/)
+      select_all(attribute[1] => args)
+    elsif attribute = name.to_s.match(/^select_all_where_(.*)_is_in$/)
+      puts args[1]
+      puts args[0]
+      select_all(:name => attribute[1].to_sym, :interval => {:min => args[0], :max => args[1]})
+    end
+
+  end #method_missing
+
 end #Array
 
 a = Array.new(["a", "aa", "Deeo", "odeo","hh","hello"])
@@ -45,5 +63,10 @@ puts a.select_all(:size => 4)
 puts a.select_all(:size => [4,2])
 puts "INTERVAL ********"
 puts a.select_all(:name => :size, :interval =>{:min => 2, :max => 4})
-puts"TEST *****"
+puts"SELECT_FIRST *****"
 puts a.select_first(:size => 2)
+puts"METHOD_MISSING"
+puts a.select_first_where_size_is(4)
+puts a.select_first_where_size_is_in(2,4)
+puts a.select_all_where_size_is(4)
+puts a.select_all_where_size_is_in(2,4)
