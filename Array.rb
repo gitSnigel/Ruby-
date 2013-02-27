a51ef5a23ecd147ff86e562797300a2821580f5a
gitSnigel <romanov.william@gmail.com>
2013-02-27 10:40:18 +0100

    finare method missing

--- Array.rb
+++ Array.rb
@@ -3,6 +3,7 @@ class Array
   def select_all(argument = {})
     raise TypeError, "You have to have less args" unless argument.size <3
   collect = Array.new()
+#    puts argument
     if argument.size == 1
       if argument.values.first.class == Array
         self.each{
@@ -20,12 +21,12 @@ class Array
       }
       end
    else
-     # puts argument
-      min = argument.values.first.values.first
-      max = argument.values.first.values.last
+      puts argument
+      min = argument.values.last.values.first
+      max = argument.values.last.values.last
       self.each{
         |x|
-        if  x.send(argument.values.last) >= min && x.send(argument.values.last) <= max
+        if  x.send(argument.values.first) >= min && x.send(argument.values.first) <= max
           collect << x
         end
       }
@@ -40,15 +41,11 @@ class Array
 
   def method_missing(name, *args)
 
-    if attribute = name.to_s.match(/^select_first_where_(.*)_is$/)
-       select_first(attribute[1].to_sym => args)
-    elsif attribute = name.to_s.match(/^select_first_where_(.*)_is_in$/)
-      select_first(:name => attribute[1].to_sym, :interval => {:min => args[0], :max => args[1]})
-    elsif  attribute = name.to_s.match(/^select_all_where_(.*)_is$/)
-      select_all(attribute[1] => args)
-    elsif attribute = name.to_s.match(/^select_all_where_(.*)_is_in$/)
-      select_all(:name => attribute[1].to_sym, :interval => {:min => args[0], :max => args[1]})
-    end
+    if attribute = name.to_s.match(/^(.*)_where_(.*)_is$/)
+      send attribute[1].to_sym, attribute[2].to_sym => args
+    elsif attribute = name.to_s.match(/^(.*)_where_(.*)_is_in$/)
+      send attribute[1].to_sym, attribute[2].to_sym => args
+     end
 
   end #method_missing
 
@@ -65,5 +62,6 @@ puts a.select_first(:size => 2)
 puts"METHOD_MISSING"
 puts a.select_first_where_size_is(4)
 puts a.select_first_where_size_is_in(2,4)
-puts a.select_all_where_size_is(4)
-puts a.select_all_where_size_is_in(2,4)
+#puts a.select_first_where_size_is_in(2,4)
+#puts a.select_all_where_size_is(4)
+#puts a.select_all_where_size_is_in(2,4)
